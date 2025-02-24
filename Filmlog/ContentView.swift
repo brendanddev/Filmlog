1b318affb1e4120f285f41dfbc4c46f6bc81b056
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-02-24 17:23:33 -0500

    Commit #1 - Initializing the Project and Enviroment

---
+++ Filmlog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Filmlog
+//
+//  Created by Brendan Dileo on 2025-02-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
